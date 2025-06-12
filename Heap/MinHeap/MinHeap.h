67c777a6398d68f05c5c1ec0c098195598c2dd01
Muhammad Faiz <m.faizmuhammad2017@gmail.com>
2025-06-12 19:59:31 +0500

    Added Sorting Fn

--- Heap/MinHeap/MinHeap.h
+++ Heap/MinHeap/MinHeap.h
@@ -64,6 +64,16 @@ public:
         }
     }
 
+    void heapSort() {
+        int originalSize = currentIndex;
+        for (int i = originalSize; i > 1; i--) {
+            swap(heap[1], heap[i]);
+            currentIndex--;
+            reHeapifyDownward(1);
+        }
+        currentIndex = originalSize; // Restore the original size after sorting
+    }
+
     void printHeap() const {
         cout << endl;
         for (int i = 1; i <= currentIndex; i++) {
